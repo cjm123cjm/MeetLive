9e97ae3fba7a34a337800065d1644ec709b80b74
chenjiamei <2671433141@qq.com>
2026-01-12 13:58:03 +0800

    electron init 搭建

---
+++ src/Front/meetlive-front/src/renderer/src/router/index.js
@@ -0,0 +1,16 @@
+import {
+  createRouter,
+  createWebHashHistory
+} from "vue-router";
+
+const router = createRouter({
+  mode: 'hash',
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [{
+    path: '/',
+    name: '登录',
+    component: () => import('@/views/Login/Login.vue')
+  }]
+})
+
+export default router
